92285b888e6074a851b3bd3a91c1f46874026d7e
QuangTien <nguyenquangtienvn2022@gmail.com>
2025-03-23 16:46:35 +0700

    load balancer

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-// app.js
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const morgan = require('morgan');
+const helmet = require('helmet');
 const routes = require('./routes/index');
+const errorHandler = require('./middleware/errorHandler');
 
 dotenv.config();
 
@@ -13,18 +14,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(helmet()); // Tăng cường bảo mật
 
 // Routes
 app.use('/api', routes);
+app.get('/', (req, res) => {
+    res.send(`Hello from backend running on port ${process.env.PORT || 3001}`);
+  });
 
-// Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ message: 'Something went wrong!' });
-});
+// Middleware xử lý lỗi chung
+app.use(errorHandler);
 
-// Start the server
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+module.exports = app;
